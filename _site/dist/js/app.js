96c423556ea143349f5d68f7dc6e7392184f21be
Keenan Payne <kpayne@slackrmedia.com>
2018-12-08 16:28:52 -0800

    Add reading time for articles

--- _site/dist/js/app.js
+++ _site/dist/js/app.js
@@ -1,3 +1,41 @@
 "use strict";
 
-console.log('testing');
+/**
+ * ReadingTime()
+ *
+ * Calculates the reading time for any given article and outputs that as a
+ * reading time for the user.
+ */
+var ReadingTime = function ReadingTime() {
+  /**
+   * @function calculateReadingTime()
+   */
+  function calculateReadingTime() {
+    var numberWords = document.querySelector('.article-content').innerHTML.split(' ').length;
+    var wordLength = numberWords / 200;
+    return Math.floor(wordLength);
+  }
+  /**
+   * @function outputReadingTime()
+   */
+
+
+  function outputReadingTime() {
+    var element = document.querySelector('[data-read-time]');
+    var time = calculateReadingTime();
+
+    if (element) {
+      element.textContent = time;
+    }
+  }
+
+  return outputReadingTime();
+};
+/**
+ * Initialize
+ */
+
+
+window.onload = function () {
+  var readTime = new ReadingTime();
+};
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,38 @@
-console.log('testing');
+/**
+ * ReadingTime()
+ *
+ * Calculates the reading time for any given article and outputs that as a
+ * reading time for the user.
+ */
+const ReadingTime = function() {
+  /**
+   * @function calculateReadingTime()
+   */
+  function calculateReadingTime() {
+  	const numberWords = document.querySelector('.article-content').innerHTML.split(' ').length;
+  	const wordLength = (numberWords / 200);
+
+  	return Math.floor(wordLength);
+  }
+
+  /**
+   * @function outputReadingTime()
+   */
+  function outputReadingTime() {
+    const element = document.querySelector('[data-read-time]');
+    const time = calculateReadingTime();
+
+    if (element) {
+      element.textContent = time;
+    }
+  }
+
+  return outputReadingTime();
+};
+
+/**
+ * Initialize
+ */
+window.onload = function() {
+  const readTime = new ReadingTime;
+};
